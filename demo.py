570997c1a7493d7a702d194b4dc46607e15d4e8d
Augustin MORVAL <augustin.morval.pro@gmail.com>
2024-10-01 15:45:46 +0200

    gradio demo

---
+++ demo.py
@@ -0,0 +1,47 @@
+import gradio as gr
+from pyscript import Transcriptor
+
+demo = gr.Blocks()
+transcriptor = Transcriptor("large-v3-turbo")
+
+microphone_transcribe = gr.Interface(
+    fn=transcriptor.transcribe_audio,
+    inputs=[
+        gr.Audio(sources="microphone", type="filepath", label="Microphone"),
+        gr.Radio([True, False], value=True, label="Enable audio enhancement"),
+    ],
+    outputs=[
+        gr.Textbox(label="Transcription"),
+        # gr.File(label="Download Transcription"),
+        # gr.Textbox(label="Console Output", lines=10)
+    ],
+    title="Audio-Transcription leveraging Whisper Model",
+    description=(
+        "Transcribe microphone recording or audio inputs and return the transcription with speaker diarization."
+    ),
+    allow_flagging="never",
+)
+
+file_transcribe = gr.Interface(
+    fn=transcriptor.transcribe_audio,
+    inputs=[
+        gr.Audio(sources="upload", type="filepath", label="Audio file"),
+        gr.Radio([True, False], value=True, label="Enable audio enhancement"),
+    ],
+    outputs=[
+        gr.Textbox(label="Transcription"),
+        # gr.File(label="Download Transcription"),
+        # gr.Textbox(label="Console Output", lines=10)
+    ],
+    title="Audio-Transcription leveraging Whisper Model",
+    description=(
+        "Transcribe microphone recording or audio inputs and return the transcription with speaker diarization."
+    ),
+    allow_flagging="never",
+)
+
+
+with demo:
+    gr.TabbedInterface([microphone_transcribe, file_transcribe], ["Microphone", "Audio file"])
+
+demo.queue().launch()
